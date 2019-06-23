0a09f55b3d29502c99e92394915e385ee8c184dc
Tchoko Apps <tchokoapps@gmail.com>
2019-06-23 13:49:55 +0200

    PropertyConfig class added

--- dependency-injection-demo/src/main/java/com/tchokoapps/springboot/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
+++ dependency-injection-demo/src/main/java/com/tchokoapps/springboot/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
@@ -4,6 +4,7 @@ import com.tchokoapps.springboot.dependencyinjectiondemo.controllers.Constructor
 import com.tchokoapps.springboot.dependencyinjectiondemo.controllers.MyController;
 import com.tchokoapps.springboot.dependencyinjectiondemo.controllers.PropertyInjectedController;
 import com.tchokoapps.springboot.dependencyinjectiondemo.controllers.SetterInjectedController;
+import com.tchokoapps.springboot.dependencyinjectiondemo.examplebeans.FakeDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -15,23 +16,27 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.tchokoapps.springboot.dependencyinjectiondemo"})
 public class DependencyInjectionDemoApplication {
 
-	private final static Logger logger = LoggerFactory.getLogger(DependencyInjectionDemoApplication.class);
+    private final static Logger logger = LoggerFactory.getLogger(DependencyInjectionDemoApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ConfigurableApplicationContext ctx = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
 
-		MyController myController = (MyController) ctx.getBean("myController");
-		logger.info(myController.hello());
+        MyController myController = (MyController) ctx.getBean("myController");
+        logger.info(myController.hello());
 
-		PropertyInjectedController propertyInjectedController = ctx.getBean(PropertyInjectedController.class);
-		logger.info(propertyInjectedController.sayHello());
+        PropertyInjectedController propertyInjectedController = ctx.getBean(PropertyInjectedController.class);
+        logger.info(propertyInjectedController.sayHello());
 
-		SetterInjectedController setterInjectedController = ctx.getBean(SetterInjectedController.class);
-		logger.info(setterInjectedController.sayHello());
+        SetterInjectedController setterInjectedController = ctx.getBean(SetterInjectedController.class);
+        logger.info(setterInjectedController.sayHello());
 
-		ConstructorInjectedController constructorInjectedController = ctx.getBean(ConstructorInjectedController.class);
-		logger.info(constructorInjectedController.sayHello());
-	}
+        ConstructorInjectedController constructorInjectedController = ctx.getBean(ConstructorInjectedController.class);
+        logger.info(constructorInjectedController.sayHello());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        logger.info(String.format("user = %s, password = %s, db_url = %s", fakeDataSource.getUrl(),
+                fakeDataSource.getPassword(), fakeDataSource.getUrl()));
+    }
 
 }
---
+++ dependency-injection-demo/src/main/java/com/tchokoapps/springboot/dependencyinjectiondemo/config/PropertyConfig.java
@@ -0,0 +1,42 @@
+package com.tchokoapps.springboot.dependencyinjectiondemo.config;
+
+import com.tchokoapps.springboot.dependencyinjectiondemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${tchoko.username}")
+    private String user;
+
+    @Value("${tchoko.password}")
+    private String password;
+
+    @Value("${tchoko.dburl}")
+    private String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
